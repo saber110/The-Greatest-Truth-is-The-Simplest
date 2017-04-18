70062b49eeab6ae1c054a5b6d05af6df56a69ad4
saber110 <huhaobin110@gmail.com>
2017-04-18 13:45:58 +0800

    fix empty and interate

--- container/eg3.4_vector_empty.c
+++ container/eg3.4_vector_empty.c
@@ -5,6 +5,27 @@ struct ST{
     int id;
     int db;
 };
+void iter_for(vector <ST>& vt)
+{
+    cout<<"iter usage"<<endl;
+    ST temp;
+    vector <ST>::iterator iter;
+    for(iter=vt.begin();iter!=vt.end();iter++)
+    {
+        temp = *iter;
+        cout<<"id: "<<temp.id<<", db: "<<temp.db<<endl;
+    }
+}
+void at_for(vector <ST> &vt)
+{
+    cout<<"at usage"<<endl;
+    ST temp;
+    for(int i=0;i<vt.size();i++)
+    {
+        temp =vt.at(i);
+        cout<<"id: "<<temp.id<<", db: "<<temp.db<<endl;
+    }
+}
 void origin(int num,vector <ST>& vt)
 {
     int m=num;
@@ -22,12 +43,21 @@ int main()
     vector <ST> myvt;
     origin(5,myvt);
     cout<<"size: "<<myvt.size()<<endl;
+
+    iter_for(myvt);
+
+    at_for(myvt);
+
+    cout<<"empty usage"<<endl;              //empty会清空容器
     while(!myvt.empty())
     {
         tmp=myvt.back();                //末尾元素
         cout<<"id "<<tmp.id<<", db "<<tmp.db<<endl;
         myvt.pop_back();      
     }
+    cout<<endl;
+    
     cin.get();
+    cout<<"bye"<<endl;
     return 1;
 }
